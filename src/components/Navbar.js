f5b02bfe73d26dc9bd65ae2f7c56e10ea0ca4961
yanimohellebi26 <yanimohellebi@gmail.com>
2025-03-16 20:51:16 +0100

    Initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/Logo.png";
+import logo from "../Assets/Logo.png"; // Ensure this path is correct
 import { Link } from "react-router-dom";
 import {
   AiOutlineHome,
