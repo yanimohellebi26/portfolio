6d5c1faa4417eef24b2bb05a5d1fae2971bda8ee
yanimohellebi26 <yani_mohellebi@etu.u-bourgogne.fr>
2025-04-03 18:23:49 +0200

    renslize

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -8,19 +8,6 @@ import {
   SiMongodb,
   SiMysql,
   SiGit,
-  SiFastapi,
-  SiDjango,
-  SiFlask,
-  SiNodedotjs,
-  SiExpress,
-  SiPostgresql,
-  SiRedis,
-  SiDocker,
-  SiKubernetes,
-  SiAws,
-  SiGooglecloud,
-  SiTensorflow,
-  SiPytorch,
 } from "react-icons/si";
 
 function Techstack() {
@@ -32,19 +19,6 @@ function Techstack() {
     { id: "mongodb", icon: <SiMongodb />, name: "MongoDB" },
     { id: "mysql", icon: <SiMysql />, name: "MySQL" },
     { id: "git", icon: <SiGit />, name: "Git" },
-    { id: "fastapi", icon: <SiFastapi />, name: "FastAPI" },
-    { id: "django", icon: <SiDjango />, name: "Django" },
-    { id: "flask", icon: <SiFlask />, name: "Flask" },
-    { id: "nodejs", icon: <SiNodedotjs />, name: "Node.js" },
-    { id: "express", icon: <SiExpress />, name: "Express" },
-    { id: "postgresql", icon: <SiPostgresql />, name: "PostgreSQL" },
-    { id: "redis", icon: <SiRedis />, name: "Redis" },
-    { id: "docker", icon: <SiDocker />, name: "Docker" },
-    { id: "kubernetes", icon: <SiKubernetes />, name: "Kubernetes" },
-    { id: "aws", icon: <SiAws />, name: "AWS" },
-    { id: "gcp", icon: <SiGooglecloud />, name: "Google Cloud" },
-    { id: "tensorflow", icon: <SiTensorflow />, name: "TensorFlow" },
-    { id: "pytorch", icon: <SiPytorch />, name: "PyTorch" },
   ];
 
   return (
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,14 +5,6 @@ import {
   SiPostman,
   SiHeroku,
   SiLinux,
-  SiDocker,
-  SiGit,
-  SiGithub,
-  SiGitlab,
-  SiJira,
-  SiTrello,
-  SiSlack,
-  SiDiscord,
 } from "react-icons/si";
 
 function Toolstack() {
@@ -21,14 +13,6 @@ function Toolstack() {
     { id: "postman", icon: <SiPostman />, name: "Postman" },
     { id: "heroku", icon: <SiHeroku />, name: "Heroku" },
     { id: "linux", icon: <SiLinux />, name: "Linux" },
-    { id: "docker", icon: <SiDocker />, name: "Docker" },
-    { id: "git", icon: <SiGit />, name: "Git" },
-    { id: "github", icon: <SiGithub />, name: "GitHub" },
-    { id: "gitlab", icon: <SiGitlab />, name: "GitLab" },
-    { id: "jira", icon: <SiJira />, name: "Jira" },
-    { id: "trello", icon: <SiTrello />, name: "Trello" },
-    { id: "slack", icon: <SiSlack />, name: "Slack" },
-    { id: "discord", icon: <SiDiscord />, name: "Discord" },
   ];
 
   return (
