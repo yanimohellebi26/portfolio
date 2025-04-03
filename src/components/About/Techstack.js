66424c044e50195f7c1ada700dba0414017cc95e
yanimohellebi26 <yani_mohellebi@etu.u-bourgogne.fr>
2025-04-03 18:14:14 +0200

    tools

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -8,17 +8,43 @@ import {
   SiMongodb,
   SiMysql,
   SiGit,
+  SiFastapi,
+  SiDjango,
+  SiFlask,
+  SiNodeDotJs,
+  SiExpress,
+  SiPostgresql,
+  SiRedis,
+  SiDocker,
+  SiKubernetes,
+  SiAws,
+  SiGooglecloud,
+  SiTensorflow,
+  SiPytorch,
 } from "react-icons/si";
 
 function Techstack() {
   const techs = [
-    { id: "c", icon: <SiC /> },
-    { id: "python", icon: <SiPython /> },
-    { id: "javascript", icon: <SiJavascript /> },
-    { id: "react", icon: <SiReact /> },
-    { id: "mongodb", icon: <SiMongodb /> },
-    { id: "mysql", icon: <SiMysql /> },
-    { id: "git", icon: <SiGit /> },
+    { id: "c", icon: <SiC />, name: "C" },
+    { id: "python", icon: <SiPython />, name: "Python" },
+    { id: "javascript", icon: <SiJavascript />, name: "JavaScript" },
+    { id: "react", icon: <SiReact />, name: "React" },
+    { id: "mongodb", icon: <SiMongodb />, name: "MongoDB" },
+    { id: "mysql", icon: <SiMysql />, name: "MySQL" },
+    { id: "git", icon: <SiGit />, name: "Git" },
+    { id: "fastapi", icon: <SiFastapi />, name: "FastAPI" },
+    { id: "django", icon: <SiDjango />, name: "Django" },
+    { id: "flask", icon: <SiFlask />, name: "Flask" },
+    { id: "nodejs", icon: <SiNodeDotJs />, name: "Node.js" },
+    { id: "express", icon: <SiExpress />, name: "Express" },
+    { id: "postgresql", icon: <SiPostgresql />, name: "PostgreSQL" },
+    { id: "redis", icon: <SiRedis />, name: "Redis" },
+    { id: "docker", icon: <SiDocker />, name: "Docker" },
+    { id: "kubernetes", icon: <SiKubernetes />, name: "Kubernetes" },
+    { id: "aws", icon: <SiAws />, name: "AWS" },
+    { id: "gcp", icon: <SiGooglecloud />, name: "Google Cloud" },
+    { id: "tensorflow", icon: <SiTensorflow />, name: "TensorFlow" },
+    { id: "pytorch", icon: <SiPytorch />, name: "PyTorch" },
   ];
 
   return (
@@ -26,6 +52,7 @@ function Techstack() {
       {techs.map((tech) => (
         <Col xs={4} md={2} className="tech-icons" key={tech.id}>
           {tech.icon}
+          <p className="tech-name">{tech.name}</p>
         </Col>
       ))}
     </Row>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,14 +5,30 @@ import {
   SiPostman,
   SiHeroku,
   SiLinux,
+  SiDocker,
+  SiGit,
+  SiGithub,
+  SiGitlab,
+  SiJira,
+  SiTrello,
+  SiSlack,
+  SiDiscord,
 } from "react-icons/si";
 
 function Toolstack() {
   const tools = [
-    { id: "vscode", icon: <SiVisualstudiocode /> },
-    { id: "postman", icon: <SiPostman /> },
-    { id: "heroku", icon: <SiHeroku /> },
-    { id: "linux", icon: <SiLinux /> },
+    { id: "vscode", icon: <SiVisualstudiocode />, name: "VS Code" },
+    { id: "postman", icon: <SiPostman />, name: "Postman" },
+    { id: "heroku", icon: <SiHeroku />, name: "Heroku" },
+    { id: "linux", icon: <SiLinux />, name: "Linux" },
+    { id: "docker", icon: <SiDocker />, name: "Docker" },
+    { id: "git", icon: <SiGit />, name: "Git" },
+    { id: "github", icon: <SiGithub />, name: "GitHub" },
+    { id: "gitlab", icon: <SiGitlab />, name: "GitLab" },
+    { id: "jira", icon: <SiJira />, name: "Jira" },
+    { id: "trello", icon: <SiTrello />, name: "Trello" },
+    { id: "slack", icon: <SiSlack />, name: "Slack" },
+    { id: "discord", icon: <SiDiscord />, name: "Discord" },
   ];
 
   return (
@@ -20,6 +36,7 @@ function Toolstack() {
       {tools.map((tool) => (
         <Col xs={4} md={2} className="tech-icons" key={tool.id}>
           {tool.icon}
+          <p className="tech-name">{tool.name}</p>
         </Col>
       ))}
     </Row>
